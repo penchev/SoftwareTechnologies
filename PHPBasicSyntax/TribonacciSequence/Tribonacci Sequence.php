ab1e8220f4ed004415bb4ab6a0dd630fe45822e4
Boyan Penchev <boyan.penchev@gmail.com>
2017-11-22 12:29:01 +0200

    Added first PHP Homework - Basic Syntax

---
+++ PHPBasicSyntax/TribonacciSequence/Tribonacci Sequence.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+
+</head>
+<body>
+    <form>
+        N: <input type="text" name="num" />
+        <input type="submit" />
+    </form>
+    <?php
+        if(isset($_GET['num'])){
+            $num = intval($_GET['num']);
+            $num1 = 0;
+            $num2 = 0;
+            $num3 = 1;
+            $tribonacci = 1;
+            for($i=0;$i<$num;$i++){
+                echo $tribonacci . ' ';
+                $tribonacci = $num1+$num2+$num3;
+                $num1 = $num2;
+                $num2 = $num3;
+                $num3 = $tribonacci;
+
+            }
+        }
+    ?>
+</body>
+</html>
